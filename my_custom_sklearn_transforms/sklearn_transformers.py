57ba64759dcfa5ed51269153cf9d7890b704630d
RFarias321 <51060029+RFarias321@users.noreply.github.com>
2020-08-31 14:49:21 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,7 +1,4 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
-
-# All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
@@ -10,22 +7,16 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
-# Instanciando uma transformação DropColumns
-rm_columns = DropColumns(
-    columns=["NOME", "MATRICULA","INGLES"]  # Essa transformação recebe como parâmetro uma lista com os nomes das colunas indesejadas
-)
-
-# Aplicando a transformação ``DropColumns`` ao conjunto de dados base
-rm_columns.fit(X=df_data_1)
-
-# Reconstruindo um DataFrame Pandas com o resultado da transformação
-df_data_2 = pd.DataFrame.from_records(
-    data=rm_columns.transform(
-        X=df_data_1
-    ),
-)
+    rm_columns = DropColumns(
+        columns=["NOME", "MATRICULA","INGLES"]
+    )
+    rm_columns.fit(X=df_data_1)
+    
+    df_data_2 = pd.DataFrame.from_records(
+        data=rm_columns.transform(
+            X=df_data_1
+        ),
+    )
