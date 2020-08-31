a594a8604de49c51f6683b05eb2866864329e7f3
RFarias321 <51060029+RFarias321@users.noreply.github.com>
2020-08-31 14:11:00 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -8,9 +8,24 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+# Instanciando uma transformação DropColumns
+rm_columns = DropColumns(
+    columns=["NOME", "MATRICULA","INGLES"]  # Essa transformação recebe como parâmetro uma lista com os nomes das colunas indesejadas
+)
+
+# Aplicando a transformação ``DropColumns`` ao conjunto de dados base
+rm_columns.fit(X=df_data_1)
+
+# Reconstruindo um DataFrame Pandas com o resultado da transformação
+df_data_2 = pd.DataFrame.from_records(
+    data=rm_columns.transform(
+        X=df_data_1
+    ),
+)
